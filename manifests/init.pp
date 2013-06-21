38f8c15ae1c68086775a60aa37db553e47fd23c2
Jon Scott <jon@jonscott.co.uk>
2013-06-21 15:02:08 +0100

    split base into own file

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,48 +18,11 @@
 #  }
 #
 
-class mongodbbase(
-  $repository = $mongodb::params::repository,
-  $package = $mongodb::params::package
-) inherits mongodb::params {
-  if !defined(Package["python-software-properties"]) {
-    package { "python-software-properties":
-      ensure => installed,
-    }
-  }
-
-  exec { "10gen-apt-repo":
-    path => "/bin:/usr/bin",
-    command => "echo '${repository}' >> /etc/apt/sources.list",
-    unless => "cat /etc/apt/sources.list | grep 10gen",
-    require => Package["python-software-properties"],
-  }
-
-  exec { "10gen-apt-key":
-    path => "/bin:/usr/bin",
-    command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
-    unless => "apt-key list | grep 10gen",
-    require => Exec["10gen-apt-repo"],
-  }
-
-  exec { "update-apt":
-    path => "/bin:/usr/bin",
-    command => "apt-get update",
-    unless => "ls /usr/bin | grep mongo",
-    require => Exec["10gen-apt-key"],
-  }
-
-  package { $package:
-    ensure => installed,
-    require => Exec["update-apt"],
-  }
-}
-
 class mongodb(
   $replSet = $mongodb::params::replSet,
   $ulimit_nofile = $mongodb::params::ulimit_nofile,
   $enable = true
-) inherits mongodbbase {
+) inherits mongodb::base {
   service { "mongodb":
     enable => $enable,
     ensure => "running",
